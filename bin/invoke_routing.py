4e28fc96fa47859e2efd7dad9aef11d6b6af84ec
Björn Otto <bjoern.otto@ovgu.de>
2026-06-01 18:33:16 +0200

    Use routing docker image

--- bin/invoke_routing.py
+++ bin/invoke_routing.py
@@ -1,14 +1,13 @@
 import requests
 
 response = requests.post(
-    "http://localhost:4000/api/route",
+    "http://localhost:4000/api/routing/ranked-routes",
     json={
-        "origin": [52.10340121158378, 11.612077698233252],  # Uni Klinik
-        "destination": [52.13054700114789, 11.642084679359717],  # Allee Center
-        "agent": "altruistic",
-        "pois": True,
+        "start": {"lat": 52.10340121158378, "lon": 11.612077698233252},  # Uni Klinik
+        "stop": {"lat": 52.13054700114789, "lon": 11.642084679359717},  # Allee Center
+        "cognitive_passport": {},
     },
 )
 
-response.raise_for_status()
 print(response.json())
+response.raise_for_status()
